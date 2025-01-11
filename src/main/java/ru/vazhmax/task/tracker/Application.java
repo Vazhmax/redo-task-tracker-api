999b9660113cd043155c825f77da92bc55588976
Vazhmax <Vazhmax0204@gmail.com>
2025-01-11 19:55:32 +0500

    refactor(feat): add ModelMapper dependency

--- src/main/java/ru/vazhmax/task/tracker/Application.java
+++ src/main/java/ru/vazhmax/task/tracker/Application.java
@@ -1,12 +1,19 @@
 package ru.vazhmax.task.tracker;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.ui.ModelMap;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }

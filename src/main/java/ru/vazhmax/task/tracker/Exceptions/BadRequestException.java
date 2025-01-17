22d56727d30800a2310844d5158d03d881923201
Vazhmax <Vazhmax0204@gmail.com>
2025-01-17 20:57:14 +0500

    feat(feat): add new exceptions

---
+++ src/main/java/ru/vazhmax/task/tracker/Exceptions/BadRequestException.java
@@ -0,0 +1,11 @@
+package ru.vazhmax.task.tracker.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public final class BadRequestException extends RuntimeException{
+    public BadRequestException(String message){
+        super(message);
+    }
+}
